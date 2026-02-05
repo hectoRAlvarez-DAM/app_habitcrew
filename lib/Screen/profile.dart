21a7d0f0bc1edcc54e481a713418b591a8df1ec3
Héctor Aznar Quirós <hectoraznar@ceroca.cat>
2026-02-05 21:10:45 +0100

    Menu Funcional

---
+++ lib/Screen/profile.dart
@@ -0,0 +1,19 @@
+import 'package:app_habitcrew/Widgets/bottomMenu.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: BottomMenu(currentIndex: 4),
+      ),
+    );
+  }
+}
---
+++ lib/Screen/quests.dart
@@ -0,0 +1,19 @@
+import 'package:app_habitcrew/Widgets/bottomMenu.dart';
+import 'package:flutter/material.dart';
+
+class Quests extends StatelessWidget {
+  const Quests({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: BottomMenu(currentIndex: 1),
+      ),
+    );
+  }
+}
