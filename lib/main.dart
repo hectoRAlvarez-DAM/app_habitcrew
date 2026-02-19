071c9e24565cbb04ef7b74d9f674620b30f1fa41
Héctor Aznar Quirós <hectoraznar@ceroca.cat>
2026-02-19 16:37:47 +0100

    Gitignore amb pubsec.lock afegit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_habitcrew/Screen/archievements_page.dart';
 import 'package:app_habitcrew/Screen/home.dart';
 import 'package:flutter/material.dart';
 import 'Screen/login_screen.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
-      home: const LoginScreen(),
+      home: const AchievementsPage(),
     );
   }
 }
