b81a8dbaf719a0c14a2e828020bd7eeab5d7ddee
Héctor Alvarez Pérez <hector_alvarez@ceroca.cat>
2026-02-19 15:39:53 +0100

    retoque_login

--- lib/main.dart
+++ lib/main.dart
@@ -11,51 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Habit Crew',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: const Color(0xFF58CC02),
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Roboto',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w900,
-            color: Color(0xFF1A1A1A),
-          ),
-          displayMedium: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w800,
-            color: Color(0xFF1A1A1A),
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.w500,
-            color: Color(0xFF4A4A4A),
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.0),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: const Color(0xFFF2F2F7),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 32.0),
-            textStyle: const TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w800,
-              letterSpacing: 0.8,
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-          ),
-        ),
-      ),
       home: const LoginScreen(),
     );
   }
